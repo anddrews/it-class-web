3ba7fa28df27a3dbb1a3f84e2523fb555d775d8f
Kondratenko Andrey <anddrews@tut.by>
2019-02-05 22:48:01 +0300

    renamed folder

---
+++ diamonds/libs/js/main.js
@@ -0,0 +1,15 @@
+ 
+ 
+  	$(".diamond").diamonds({
+
+        size : 290, // Size of diamonds in pixels. Both width and height. 
+        gap : 8, // Pixels between each square.
+        hideIncompleteRow : false, // Hide last row if there are not enough items to fill it completely.
+        autoRedraw : true, // Auto redraw diamonds when it detects resizing.
+        itemSelector : ".item-diamond" // the css selector to use to select diamonds-items.
+ }); 
+ 
+     
+   
+
+
